31ffe39c5cf6e6a4be3e19173af333f44ba0d382
Terry Son <sakpung@hanmail.net>
2017-08-29 14:34:04 +0900

    스터디중

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python Test Project
 Python을 배우면서 만들어진 source를 등록 관리한다
+
+
+
+https://docs.python.org/3.5/tutorial/index.html
+
+https://docs.python.org/3.5/tutorial/controlflow.html  -> 4번 진행 중
--- TestCode/test.py
+++ TestCode/test.py
@@ -1,5 +1,31 @@
-the_world_is_flat = True
+# 이거는 주서이다.
+the_world_is_flat = False # True  # 이렇게 주석을 달 수 있다.
+text = "#는 주석이 아니고 문자열이다"
+변수 = "한글 변수명도 된다."
+print("출력은 디버그 콘솔에서 출력 된다")
 if the_world_is_flat :
-    print(" be careful not fall off!")
+    print("be careful not fall off!" + "\n" + text)
+    print( 변수 )
 
-    
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+print( 3 * "un" + "test")
+
+print( text[4:8] )
+print( text[2:] )
+print( text[:8] )
+# print( "문자열 길이 : " + len(text) )
+print( "문자열 길이 : %d" %  len(text) )
+# print( "문자열 길이 : %d %s" % {len(text) % text} ) # error 발생
+print( "UTF-8을 기본적으로 사용하여 한글도 1byte처럼 사용된다" )
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a+b
+
+quit    
