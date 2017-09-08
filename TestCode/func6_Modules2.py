4c9dc07040e5cb5e5af42347b01754703832a953
Terry Son <sakpung@hanmail.net>
2017-09-08 16:54:28 +0900

    test 진행 중

---
+++ TestCode/func6_Modules2.py
@@ -0,0 +1,12 @@
+# fibonacci numbers module
+
+import func6_Modules
+
+func6_Modules.fib( 1000 )
+
+print( func6_Modules.fib2( 1000) )
+
+print('__name__', func6_Modules.__name__ )
+print('__doc__', func6_Modules.__doc__ )
+print( '__file__', func6_Modules.__file__ )
+print( '__package__', func6_Modules.__package__ )
