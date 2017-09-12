fc54c0cc47cd6fed46a7fc917c8980c6579fb458
Terry Son <sakpung@hanmail.net>
2017-09-12 10:49:15 +0900

    test 중 6.4 진행...

---
+++ TestCode/func6.2_standard.py
@@ -0,0 +1,8 @@
+print( '6.2 Standard Modules', '시작' )
+print( '표준 모듈' )
+import sys
+
+# print( sys.ps1 )  => interpreter환경에서 구동됨
+# print( sys.ps2 )  => interpreter환경에서 구동됨
+
+print( '6.2 Standard Modules', '완료' )
